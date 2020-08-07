62e3cc598efdf7f6ace04d0e97699ad0292511bf
manufactory-dave <69147972+manufactory-dave@users.noreply.github.com>
2020-08-07 16:16:51 +0100

    ABL tweaks and recompile
    Reduced ABL to 3x3 grid with no second measurement
    
    Recompiled binary

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -972,7 +972,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 2
+//#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
@@ -1243,7 +1243,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
+  #define GRID_MAX_POINTS_X 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
